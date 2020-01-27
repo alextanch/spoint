822601621bd13e09c75d32419ec656a109675ac3
Alexander Tanchenko <aletan@protonmail.com>
2020-01-27 21:34:28 +0300

    add random brightness aug

--- spoint/utils/augmentation.py
+++ spoint/utils/augmentation.py
@@ -8,7 +8,8 @@ class Augmentation:
         self.compose = Compose([
             Resize(size),
             GaussianNoise(**config.gaussian_noise) if 'gaussian_noise' in config else None,
-            SpeckleNoise(**config.speckle_noise) if 'speckle_noise' in config else None
+            SpeckleNoise(**config.speckle_noise) if 'speckle_noise' in config else None,
+            RandomBrightness(**config.random_brightness) if 'random_brightness' in config else None
         ])
 
     def __call__(self, image):
@@ -62,9 +63,31 @@ class SpeckleNoise:
         if random.random() < self.prob:
             p = np.random.uniform(*self.stddev)
             sample = np.random.uniform(size=image.shape)
+
             noisy_image = np.where(sample <= p, np.zeros_like(image), image)
             noisy_image = np.where(sample >= (1. - p), 255. * np.ones_like(image), noisy_image)
 
         return noisy_image.astype(np.uint8)
+
+
+class RandomBrightness:
+    def __init__(self, prob=0.5, max_change=50):
+        self.prob = prob
+        self.max_change = max_change
+
+    def __call__(self, image):
+        if random.random() < self.prob:
+            value = np.random.uniform(-self.max_change,  self.max_change)
+            image = np.clip(image + value, 0, 255)
+
+        return image.astype(np.uint8)
+
+
+# def random_brightness(image, max_abs_change=50):
+#     import tensorflow as tf
+#     return tf.clip_by_value(tf.image.random_brightness(image, max_abs_change), 0, 255)
+#     sess = tf.Session()
+#     with sess.as_default():
+#         _img = img.val()
 
 

b2d985fb4b10e4f1da43e49914eb7e5d57c24d17
Alexander Tanchenko <aletan@protonmail.com>
2020-01-28 09:41:17 +0300

    inherit SuperPointLoss from nn.Module

--- spoint/losses/superpoint_loss.py
+++ spoint/losses/superpoint_loss.py
@@ -1,26 +1,30 @@
 import torch.nn as nn
 
 
-class SuperPointLoss:
+class SuperPointLoss(nn.Module):
     def __init__(self, config, device):
+        super().__init__()
+
         self.config = config
         self.device = device
 
+        self.bce_loss = nn.BCELoss(reduction='none').to(self.device)
+
     def detector_loss(self, input, target, mask):
-        bce_loss = nn.BCELoss(reduction='none').to(self.device)
-        loss = bce_loss(nn.functional.softmax(input, dim=1), target)
+        soft_max = nn.functional.softmax(input, dim=1)
+        loss = self.bce_loss(soft_max, target)
 
         loss = loss.sum(dim=1)
 
-        if mask:
+        if mask is None:
+            loss = loss.mean()
+        else:
             loss = (loss * mask).sum()
             loss = loss / (mask.sum() + 1e-10)
-        else:
-            loss = loss.mean()
 
         return loss
 
-    def __call__(self, input, target, mask=None):
+    def forward(self, input, target, mask=None):
         loss = self.detector_loss(input, target, mask)
 
         return loss
